af8f1c247ebdaf4b54e99c959ea93957237648a9
guilderguzman <guilderguzman9@gmail.com>
2026-06-12 11:36:04 -0700

    fix the server

--- metaverse-backend/server.js
+++ metaverse-backend/server.js
@@ -57,7 +57,7 @@ require("./app/routes/auth.routes")(app);
 require("./app/routes/user.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
