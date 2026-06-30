01d040173e272cd69b68a7f6af5e7427ecdccefe
guilderguzman <guilderguzman9@gmail.com>
2026-06-30 15:54:28 -0700

    update the server

--- metaverse-backend/server.js
+++ metaverse-backend/server.js
@@ -34,18 +34,6 @@ app.use(
   })
 );
 
-// database
-// const db = require("./app/models");
-// const Role = db.role;
-
-// db.sequelize.sync({ alter: true }).then(() => {
-//   initial();
-// });
-// force: true will drop the table if it already exists
-// db.sequelize.sync({force: true}).then(() => {
-//   console.log('Drop and Resync Database with { force: true }');
-//   initial();
-// });
 
 // simple route
 app.get("/", (req, res) => {
